7d68db31bd8d08eb43f905ad29e10cf0ad8f0a56
Ebrahim Rt <ebrahim.rayatparvar@hotmail.com>
2021-09-07 18:14:25 +0430

    ایجاد گلاس کار با تصویر و پیاده سازی متد تبدیل عکس به باینری

---
+++ ZangClass Extension/ConvertImage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+
+namespace ZangClass_Extension
+{
+    public static class ConvertImage
+    {
+        /// <summary>
+        /// تبدیل عکس به باینری
+        /// </summary>
+        /// <param name="img">ورودی عکس</param>
+        /// <returns>خروجی باینری</returns>
+        public static byte[] ConvertImageToByte(Image img)  //--------- تبدیل عکس به باینری -------------
+        {
+            byte[] byteArray;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, ImageFormat.Png);
+                stream.Close();
+                byteArray = stream.ToArray();
+            }
+            return byteArray;
+        }
+    }
+}
