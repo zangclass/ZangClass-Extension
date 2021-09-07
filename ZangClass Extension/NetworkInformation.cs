930ab1a100fa24294f1e9b126ca59e75db88cc30
Ebrahim Rt <ebrahim.rayatparvar@hotmail.com>
2021-09-07 18:31:05 +0430

    بدست آوردن آدرس ip سیستم جاری

--- ZangClass Extension/NetworkInformation.cs
+++ ZangClass Extension/NetworkInformation.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace ZangClass_Extension
 {
     public static class NetworkInformation
     {
+        /// <summary>
+        /// بدست آوردن آدرس آی پی سیستم جاری
+        /// </summary>
+        /// <returns>خروجی آدرس IP</returns>
+        public static string GetIpAddress()
+        {
+            //return new IPHostEntry().AddressList[0].ToString();
+            return Dns.GetHostEntry(Dns.GetHostName())
+                .AddressList.First(f => f.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                .ToString();
+        }
     }
 }
